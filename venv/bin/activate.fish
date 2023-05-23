49d4832280d32ef853d67483510002e46a558d6a
ShivaliSKirdat <shivali.kirdat@bizmetric.com>
2023-05-23 06:13:06 +0000

    commiting jenkinsfile to git repo

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/web_deploy/venv"
+set -gx VIRTUAL_ENV "/home/shivali/web_deploy/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
