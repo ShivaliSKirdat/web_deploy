780f0e0aa86f58e6bc637622a97d1e255b9816cb
ShivaliSKirdat <80674087+ShivaliSKirdat@users.noreply.github.com>
2023-06-12 14:45:18 +0530

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,20 +1,20 @@
-from os import environ as env
-from azure.identity import ClientSecretCredential
-from azure.keyvault.secrets import SecretClient
+# from os import environ as env
+# from azure.identity import ClientSecretCredential
+# from azure.keyvault.secrets import SecretClient
 
-TENANT_ID = env.get("AZURE_TENANT_ID", "")
-CLIENT_ID = env.get("AZURE_CLIENT_ID", "")
-CLIENT_SECRET = env.get("AZURE_CLIENT_SECRET", "")
+# TENANT_ID = env.get("AZURE_TENANT_ID", "")
+# CLIENT_ID = env.get("AZURE_CLIENT_ID", "")
+# CLIENT_SECRET = env.get("AZURE_CLIENT_SECRET", "")
 
-KEYVAULT_NAME = env.get("AZURE_KEYVAULT_NAME", "")
-KEYVAULT_URI = f"https://{KEYVAULT_NAME}.vault.azure.net/"
+# KEYVAULT_NAME = env.get("AZURE_KEYVAULT_NAME", "")
+# KEYVAULT_URI = f"https://{KEYVAULT_NAME}.vault.azure.net/"
 
-_credential = CLientSecretCredential(
-  tenant_id=TENANT_ID,
-  client_id=CLIENT_ID,
-  client_secret=CLIENT_SECRET
-)
+# _credential = CLientSecretCredential(
+#   tenant_id=TENANT_ID,
+#   client_id=CLIENT_ID,
+#   client_secret=CLIENT_SECRET
+# )
 
-_sc = SecretClient(vault_url=KEYVAULT_URI, credential=_credential)
-DEMO_DB_USERNAME = _sc.get_secret("admin").value
-DEMO_DB_PASSWORD = _sc.get_secret("DemoUserPassword").value
+# _sc = SecretClient(vault_url=KEYVAULT_URI, credential=_credential)
+# DEMO_DB_USERNAME = _sc.get_secret("admin").value
+# DEMO_DB_PASSWORD = _sc.get_secret("DemoUserPassword").value
